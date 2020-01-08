3b637f8909c435b31dfef261afb1cb536d7c7773
Arsenii <Fedorov-Ars@yandex.ru>
2020-01-09 00:59:38 +0300

    Release 1.0

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const middleSalary = 90000;
 const architectSalary = 70000;
 const juniorSalary = 50000;
 
+const secondsCountInMonth = 20 * 8 * 60 * 60;
+
 class Employee {
     constructor(id) {
         this.id = id;
@@ -12,27 +14,27 @@ class Employee {
         if (id < 100) {
             this.salary = chiefSalary;
             this.position = "Рук. направления";
-            this.salaryBySecond = (this.salary / 20 / 8 / 60 / 60);
+            this.salaryBySecond = (this.salary / secondsCountInMonth);
         } else if (id < 200) {
             this.salary = leadSalary;
             this.position = "Рук. группы";
-            this.salaryBySecond = (this.salary / 20 / 8 / 60 / 60);
+            this.salaryBySecond = (this.salary / secondsCountInMonth);
         } else if (id < 300) {
-            this.salary = seniorSalary;
-            this.position = "Senior";
-            this.salaryBySecond = (this.salary / 20 / 8 / 60 / 60);
-        }else if (id < 400) {
-            this.salary = middleSalary;
-            this.position = "Middle";
-            this.salaryBySecond = (this.salary / 20 / 8 / 60 / 60);
-        }else if (id < 500) {
             this.salary = architectSalary;
             this.position = "Архитектор";
-            this.salaryBySecond = (this.salary / 20 / 8 / 60 / 60);
-        }else if (id < 600) {
+            this.salaryBySecond = (this.salary / secondsCountInMonth);
+        } else if (id < 400) {
+            this.salary = seniorSalary;
+            this.position = "Senior";
+            this.salaryBySecond = (this.salary / secondsCountInMonth);
+        } else if (id < 500) {
+            this.salary = middleSalary;
+            this.position = "Middle";
+            this.salaryBySecond = (this.salary / secondsCountInMonth);
+        } else if (id < 600) {
             this.salary = juniorSalary;
             this.position = "Junior";
-            this.salaryBySecond = (this.salary / 20 / 8 / 60 / 60);
+            this.salaryBySecond = (this.salary / secondsCountInMonth);
         }
     }
 }
